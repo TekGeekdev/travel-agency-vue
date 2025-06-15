af2f2a4f30405184c50325291ba6dcf053f68ddb
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-06-15 18:19:43 -0400

    add config server

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const corsOption = {
+  origin: 'http://localhost:8089',
+  credential: true,
+};
+
+app.use(cors(corsOption));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//test working
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome' });
+});
+
+const PORT = 8088;
+app.listen(PORT, () => {
+  console.log(`server is up on port ${PORT}`);
+});
