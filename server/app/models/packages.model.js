5f1355cfab090e03fb195e20cdcf8347eb42e00f
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-06-21 21:04:06 -0400

    add validation rules in model

--- server/app/models/packages.model.js
+++ server/app/models/packages.model.js
@@ -11,21 +11,43 @@ module.exports = (connex, Sequelize) => {
       name: {
         type: Sequelize.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: 'Le nom est obligatoire.' },
+          notEmpty: { msg: 'Le nom ne peut pas être vide.' },
+        },
       },
       description: {
         type: Sequelize.TEXT,
         allowNull: false,
+        validate: {
+          notNull: { msg: 'La description est obligatoire.' },
+          notEmpty: { msg: 'La description ne peut pas être vide.' },
+        },
       },
       price: {
         type: Sequelize.DECIMAL(10, 2),
         allowNull: false,
+        validate: {
+          notNull: { msg: 'Le prix est obligatoire.' },
+          isDecimal: { msg: 'Le prix doit être un nombre décimal.' },
+        },
       },
       category: {
         type: Sequelize.STRING,
         allowNull: false,
+        validate: {
+          notNull: { msg: 'La catégorie est obligatoire.' },
+          notEmpty: { msg: 'La catégorie ne peut pas être vide.' },
+        },
       },
       url_img: {
         type: Sequelize.STRING,
+        allowNull: true,
+        validate: {
+          isUrl: {
+            msg: "L'URL de l’image doit être une URL valide.",
+          },
+        },
       },
     },
     {
