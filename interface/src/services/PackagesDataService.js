c531b9a5c136e66885ac10cacb2fdc0ca4de484b
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-06-21 21:05:15 -0400

    add route create

--- interface/src/services/PackagesDataService.js
+++ interface/src/services/PackagesDataService.js
@@ -4,6 +4,10 @@ class PackageDataService {
   getAll() {
     return http.get('/packages');
   }
+
+  create(data) {
+    return http.post('/packages', data);
+  }
 }
 
 export default new PackageDataService();
