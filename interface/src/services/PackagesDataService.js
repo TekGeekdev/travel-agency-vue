f3d1966a593301d30a6a022a6db91fbe265a989a
TekGeek_dev <TekGeek_dev@protonmail.com>
2025-06-22 00:17:51 -0400

    add endpoint update & getOne

--- interface/src/services/PackagesDataService.js
+++ interface/src/services/PackagesDataService.js
@@ -8,6 +8,14 @@ class PackageDataService {
   create(data) {
     return http.post('/packages', data);
   }
+
+  getOne(id) {
+    return http.get(`/packages/${id}`);
+  }
+
+  update(id, data) {
+    return http.put(`/packages/${id}`, data);
+  }
 }
 
 export default new PackageDataService();
